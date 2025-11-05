8d57f7717d3c17997a5a10960cac7b483c1e0dd6
Yesenia Ramirez <ramirezy0415@gmail.com>
2025-11-04 20:53:19 -0600

    feature: Adding login endpoint

--- api/users.js
+++ api/users.js
@@ -3,7 +3,7 @@ const usersRouter = express.Router();
 export default usersRouter;
 
 import requireBody from "#middleware/requireBody";
-import { createUser } from "#db/queries/users";
+import { createUser, getUser } from "#db/queries/users";
 import { createToken } from "#utils/jwt";
 
 usersRouter.post(
@@ -19,6 +19,26 @@ usersRouter.post(
       }
 
       const token = createToken({ id: registeredUser.id });
+      return res.status(201).json(token);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
+
+usersRouter.post(
+  "/login",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const userInfo = await getUser(username, password);
+
+      if (!userInfo) {
+        return res.status(401).json({ error: "Invalid user credentials" });
+      }
+
+      const token = createToken({ id: userInfo.id });
       return res.status(201).json(token);
     } catch (error) {
       console.error(error);
--- db/queries/users.js
+++ db/queries/users.js
@@ -19,3 +19,33 @@ export async function createUser(username, password) {
     console.error("Error in createUser: ", error);
   }
 }
+
+export async function getUser(username, password) {
+  try {
+    const query = `
+    SELECT *
+    FROM users
+    WHERE username = $1;`;
+    const values = [username];
+    const {
+      rows: [user],
+    } = await db.query(query, values);
+
+    // If no users with the provided username
+    if (!user) {
+      return null;
+    }
+
+    // Check if the password matches the one passed in
+    const correctPassword = bcrypt.compare(password, user.password);
+    // If not correct password
+    if (!correctPassword) {
+      return null;
+    }
+
+    // If correct password
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
